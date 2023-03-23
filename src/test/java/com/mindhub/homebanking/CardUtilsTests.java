6e323c814eaea641c0ed31340e120968a6149699
emiliasosa <emi.sosato@gmail.com>
2023-03-23 16:26:53 -0300

    comment test

--- src/test/java/com/mindhub/homebanking/CardUtilsTests.java
+++ src/test/java/com/mindhub/homebanking/CardUtilsTests.java
@@ -9,28 +9,28 @@ import static org.hamcrest.Matchers.*;
 @SpringBootTest
 public class CardUtilsTests {
 
-    @Test
-    public void cvvLess(){
-        int cardCvv = CardUtils.getCvv();
-        assertThat(cardCvv,lessThan(1000));
-    }
+//    @Test
+//    public void cvvLess(){
+//        int cardCvv = CardUtils.getCvv();
+//        assertThat(cardCvv,lessThan(1000));
+//    }
 
-    @Test
-    public void cvvGreater(){
-        int cardCvv = CardUtils.getCvv();
-        assertThat(cardCvv,greaterThan(99));
-    }
-
-    @Test
-    public void cvvEither(){
-        int cardCvv = CardUtils.getCvv();
-        assertThat(cardCvv,either(greaterThan(99)).or(lessThan(1000)));
-    }
-
-    @Test
-    public void cvvNull(){
-        int cardCvv = CardUtils.getCvv();
-        assertThat(cardCvv,notNullValue());
-    }
+//    @Test
+//    public void cvvGreater(){
+//        int cardCvv = CardUtils.getCvv();
+//        assertThat(cardCvv,greaterThan(99));
+//    }
+//
+//    @Test
+//    public void cvvEither(){
+//        int cardCvv = CardUtils.getCvv();
+//        assertThat(cardCvv,either(greaterThan(99)).or(lessThan(1000)));
+//    }
+//
+//    @Test
+//    public void cvvNull(){
+//        int cardCvv = CardUtils.getCvv();
+//        assertThat(cardCvv,notNullValue());
+//    }
 
 }
